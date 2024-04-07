7d82ff72ed30f67900b77407c8af78de978c7c0c
rogeriojr <rogeriojr1100@gmail.com>
2024-04-07 05:25:27 -0300

    add postgress

--- prisma/migrations/20240404060136_attendees_table/migration.sql
+++ prisma/migrations/20240404060136_attendees_table/migration.sql
@@ -1,9 +1,4 @@
 -- CreateTable
 CREATE TABLE "attendees" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "name" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "event_id" TEXT NOT NULL,
-    CONSTRAINT "attendees_event_id_fkey" FOREIGN KEY ("event_id") REFERENCES "events" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+    "id" serial NOT NULL PRIMARY KEY, "name" TEXT NOT NULL, "email" TEXT NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, "event_id" TEXT NOT NULL, CONSTRAINT "attendees_event_id_fkey" FOREIGN KEY ("event_id") REFERENCES "events" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
--- prisma/migrations/20240404061011_checkin/migration.sql
+++ prisma/migrations/20240404061011_checkin/migration.sql
@@ -1,27 +1,31 @@
 -- CreateTable
 CREATE TABLE "check_ins" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "attendee_id" INTEGER NOT NULL,
-    CONSTRAINT "check_ins_attendee_id_fkey" FOREIGN KEY ("attendee_id") REFERENCES "attendees" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+    "id" serial NOT NULL PRIMARY KEY, "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, "attendee_id" INTEGER NOT NULL, CONSTRAINT "check_ins_attendee_id_fkey" FOREIGN KEY ("attendee_id") REFERENCES "attendees" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- RedefineTables
-PRAGMA foreign_keys=OFF;
+PRAGMA foreign_keys = OFF;
+
 CREATE TABLE "new_attendees" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "name" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "event_id" TEXT NOT NULL,
-    CONSTRAINT "attendees_event_id_fkey" FOREIGN KEY ("event_id") REFERENCES "events" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+    "id" serial NOT NULL PRIMARY KEY, "name" TEXT NOT NULL, "email" TEXT NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, "event_id" TEXT NOT NULL, CONSTRAINT "attendees_event_id_fkey" FOREIGN KEY ("event_id") REFERENCES "events" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
-INSERT INTO "new_attendees" ("created_at", "email", "event_id", "id", "name") SELECT "created_at", "email", "event_id", "id", "name" FROM "attendees";
+
+INSERT INTO
+    "new_attendees" (
+        "created_at", "email", "event_id", "id", "name"
+    )
+SELECT "created_at", "email", "event_id", "id", "name"
+FROM "attendees";
+
 DROP TABLE "attendees";
+
 ALTER TABLE "new_attendees" RENAME TO "attendees";
-CREATE UNIQUE INDEX "attendees_event_id_email_key" ON "attendees"("event_id", "email");
+
+CREATE UNIQUE INDEX "attendees_event_id_email_key" ON "attendees" ("event_id", "email");
+
 PRAGMA foreign_key_check;
-PRAGMA foreign_keys=ON;
+
+PRAGMA foreign_keys = ON;
 
 -- CreateIndex
-CREATE UNIQUE INDEX "check_ins_attendee_id_key" ON "check_ins"("attendee_id");
+CREATE UNIQUE INDEX "check_ins_attendee_id_key" ON "check_ins" ("attendee_id");
